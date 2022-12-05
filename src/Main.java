a9f584a4a0da99edc624d2be34d0b5dd8fa376f0
Akın CENGİZ <akincengiz@outlook.com.tr>
2022-12-05 16:36:46 +0300

    LeapYear proje dosyasi eklendi.

--- src/Main.java
+++ src/Main.java
@@ -2,67 +2,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner input = new Scanner(System.in);
-        int km;
-        byte yas, yolculukTipi;
-        double normalTutar, gidisDonusIndirim = 0.2, tutar, indirimMiktari=0;
+        System.out.print("Hesaplanacak olan yılı giriniz : ");
+        int year = input.nextInt();
+        boolean isLeapYear = (year%4==0) ? true : false;
 
-        while (true)
+        if(isLeapYear == true)
         {
-            System.out.print("Mesafeyi km türünden giriniz : ");
-            km = input.nextInt();
-            if(km<0)
-            {
-                System.out.print("Hatalı veri girişi yaptınız...\n");
-                continue;
-            }
-            System.out.print("Yaşınızı giriniz : ");
-            yas = input.nextByte();
-            if(yas<0)
-            {
-                System.out.print("Hatalı veri girişi yaptınız...\n");
-                continue;
-            }
-            System.out.print("Yolculuk tipini giriniz (1 => Tek Yön - 2 => Gidiş - Dönüş) : ");
-            yolculukTipi = input.nextByte();
-            if(yolculukTipi < 1 || yolculukTipi > 2)
-            {
-                System.out.print("Hatalı veri girişi yaptınız...\n");
-                continue;
-            }
-            break;
-        }
-        normalTutar = km * 0.1;
-        if(yas<12)
+            System.out.println(year + " yılı artık yıldır...");
+        }else
         {
-            indirimMiktari = normalTutar*0.5;
-            normalTutar -= indirimMiktari;
+            System.out.println(year + " yılı artık yıl değildir...");
         }
-        else if(yas >= 12 && yas <=24)
-        {
-            indirimMiktari = normalTutar * 0.1;
-            normalTutar -= indirimMiktari;
-        }
-        else if(yas>65)
-        {
-            indirimMiktari = normalTutar*0.3;
-            normalTutar -= indirimMiktari;
-        }
-        else
-        {
-            indirimMiktari = normalTutar*0;
-            normalTutar -= indirimMiktari;
-        }
-
-        if(yolculukTipi == 2)
-        {
-            tutar = (normalTutar - (normalTutar * 0.2)) * 2;
-        }
-        else
-        {
-            tutar = normalTutar;
-        }
-        System.out.println("Tutar : "+tutar + " TL");
     }
 }
